aea2ea36b27110acbfb6ae65b9360ce77a9cc4c2
Musoxon <msx.tuit@gmail.com>
2021-10-29 19:46:10 +0500

    Foodelich app from Flutter Apprentice

---
+++ lib/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // TODO: Style the title
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      // TODO: Style the body text
+      body: Center(
+        child: Text(
+          'Let\'s get cooking 👩‍🍳',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/home.dart';
+import 'fooderlich_theme.dart';
 
 void main() {
   // 1
@@ -10,19 +12,14 @@ class Fooderlich extends StatelessWidget {
   const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // TODO: Create theme
+    final theme = FooderlichTheme.dark();
     // TODO: Apply Home widget
-    // 3
     return MaterialApp(
-      // TODO: Add theme
+      debugShowCheckedModeBanner: false,
+      theme: theme,
       title: 'Fooderlich',
       // 4
-      home: Scaffold(
-        // TODO: Style the title
-        appBar: AppBar(title: const Text('Fooderlich')),
-        // TODO: Style the body text
-        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
-      ),
+      home: const Home(),
     );
   }
 }
