beacfd8937276e9b00379c6ddb0382482b1f56cc
Musoxon <msx.tuit@gmail.com>
2021-10-29 23:39:48 +0500

    Foodelich app from Flutter Apprentice

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'card1.dart';
+import 'card2.dart';
+import 'card3.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
   @override
@@ -7,6 +11,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = [
+    const Card1(),
+    const Card2(),
+    const Card3(),
+  ];
+
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +37,22 @@ class _HomeState extends State<Home> {
         ),
       ),
       // TODO: Style the body text
-      body: Center(
-        child: Text(
-          'Let\'s get cooking 👩‍🍳',
-          style: Theme.of(context).textTheme.headline1,
-        ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card-1'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card-2'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card-3'),
+        ],
       ),
     );
   }
