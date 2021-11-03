fdfb90b6c5a954f198bd9e4a791d9ca62fe123d0
Musoxon <msx.tuit@gmail.com>
2021-11-03 23:33:27 +0500

    Foodelich app from Flutter Apprentice

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/api/mock_fooderlich_service.dart';
 
-class ExplorePage extends StatefulWidget {
-  const ExplorePage({Key? key}) : super(key: key);
+class ExplorePage extends StatelessWidget {
+  ExplorePage({Key? key}) : super(key: key);
 
-  @override
-  _ExplorePageState createState() => _ExplorePageState();
-}
+  final mockService = MockFooderlichService();
 
-class _ExplorePageState extends State<ExplorePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Center(
+      child: Text('Explore Page'),
+    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/pages/explore_page.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,8 +12,7 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0;
 
   static List<Widget> pages = <Widget>[
-    // TODO: Replace with ExploreScreen
-    Container(color: Colors.white),
+    ExplorePage(),
     // TODO: Replace with RecipesScreen
     Container(color: Colors.green),
     Container(color: Colors.blue),
